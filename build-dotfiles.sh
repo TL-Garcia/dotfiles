1b7051e6194e6e8dd7216d2142d16204aaed5f88
TL-Garcia <tomas.bl.garcia@gmail.com>
2022-01-03 17:09:21 +0100

    Implement script with .txt file

--- build-dotfiles.sh
+++ build-dotfiles.sh
@@ -2,13 +2,33 @@
 
 CONFIG_DIR="$HOME/.config"
 
-tmux_dir="$CONFIG_DIR/tmux" 
-if [[ -d "$tmux_dir" ]]; then
-	echo "Config dir already exists"
-else 
-	mkdir "$tmux_dir"
-	ln -s "$(pwd)/tmux.conf" "$tmux_dir/tmux.conf"
+manage_config() {
+  if [[ $# -ne 2 ]]
+  then
+    echo "Error: Wrong usage" >&2
+    echo "Arguments should be: <program folder> <config file name>" >&2 
+    exit 85 # Wrong arguments
+  fi
 
-	echo "Creating config dir"	
-fi
+  config_dir="$CONFIG_DIR/$1" 
+  config_file_name="$2"
+  config_file_path="$config_dir/$config_file_name"
 
+  echo "Scanning for $config_file_name file in $1 directory..."
+
+	if [[ -d "$config_dir" ]]
+  then
+		echo "Config already exists, exiting without making changes"
+	else 
+		mkdir "$config_dir"
+		ln -s "$(pwd)/$config_file_name" "$config_file_path"
+
+		echo "Creating config dir"	
+	fi
+  echo ""
+}
+
+cat file-names.txt | while IFS=: read -r config_dir config_file
+do
+  manage_config $config_dir $config_file
+done
