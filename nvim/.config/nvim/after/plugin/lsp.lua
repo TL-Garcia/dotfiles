f8b3fdc451c5a8d58617e4e4003b688f67f99087
Tomás Loroño Garcia <n461387@gs01600n461387m.wired.eut-es.corp>
2023-04-27 12:45:55 +0200

    fix: Disable treesitter in vscode-nvim

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -15,4 +15,8 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 require('lspconfig').jdtls.setup{ cmd = { 'jdtls' } }
 
-lsp.setup()
+lsp.setup({
+  highlight = {
+    enable = (not vim.g.vscode),
+  }
+})
