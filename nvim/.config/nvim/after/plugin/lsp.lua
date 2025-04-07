6f64466d068f2d7b14e1b0bf9fbb15cdf469c992
tl-garcia <tomas.bl.garcia@gmail.com>
2025-04-07 13:41:08 +0200

    nvim: Update plugins & lsp config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,25 +1,58 @@
 local lsp = require('lsp-zero')
 
-lsp.preset("recommended")
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●', -- Could be '●', '▎', 'x'
+    spacing = 4,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
 
 -- Keybindings
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({ buffer = bufnr })
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
 
-  local opts = { buffer = bufnr }
-
-  vim.keymap.set({ 'n', 'x' }, 'gq', function()
-    vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-  end, opts)
-end)
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
+})
 
 local cmp = require('cmp')
 
 cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end,
+  },
   mapping = cmp.mapping.preset.insert({
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
   }),
 })
+
 
 -- Mason (lsp management)
 require("mason").setup({
@@ -27,10 +60,9 @@ require("mason").setup({
 })
 require("mason-lspconfig").setup()
 
--- (Optional) Configure lua language server for neovim
+-- Configure lua language server for neovim
 local lspConfig = require('lspconfig')
 lspConfig.lua_ls.setup(lsp.nvim_lua_ls())
-lspConfig.jdtls.setup { cmd = { 'jdtls' } }
 
 -- Config deno/tsserver based on project file
 lspConfig.denols.setup {
@@ -68,3 +100,4 @@ require('nvim-ts-autotag').setup({
     enable_close_on_slash = false -- Auto close on trailing </
   },
 })
+
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,41 +1,50 @@
-require('lazy').setup{
+require('lazy').setup {
   -- Plugins
-  'jiangmiao/auto-pairs';
-  'tpope/vim-surround';
-  'vim-airline/vim-airline';
-  'windwp/nvim-ts-autotag';
+  'jiangmiao/auto-pairs',
+  'tpope/vim-surround',
+  'vim-airline/vim-airline',
+  'windwp/nvim-ts-autotag',
 
   -- COLOR SCHEMES
-  'Mofiqul/dracula.nvim';
-  'catppuccin/nvim';
+  'Mofiqul/dracula.nvim',
+  'catppuccin/nvim',
 
   -- Navigation
-  'junegunn/fzf';
-  'preservim/nerdtree';
-  'ryanoasis/vim-devicons';
-  'tiagofumo/vim-nerdtree-syntax-highlight';
-  'nvim-tree/nvim-web-devicons';
+  'junegunn/fzf',
+  'preservim/nerdtree',
+  'ryanoasis/vim-devicons',
+  'tiagofumo/vim-nerdtree-syntax-highlight',
+  'nvim-tree/nvim-web-devicons',
 
   {
-  'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  };
+  },
 
   -- Git
-  'tpope/vim-fugitive';
+  'tpope/vim-fugitive',
 
-  -- LSP  
-  'alvan/vim-closetag';
-  'saadparwaiz1/cmp_luasnip'; -- Snippets source for nvim-cmp;
-  'williamboman/mason.nvim';
-  'williamboman/mason-lspconfig.nvim';
-  'nvim-treesitter/nvim-treesitter';
-  {'neovim/nvim-lspconfig'},
-  {'VonHeikemen/lsp-zero.nvim'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  {'L3MON4D3/LuaSnip'},
+  -- LSP
+  'alvan/vim-closetag',
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'nvim-treesitter/nvim-treesitter',
+  { 'neovim/nvim-lspconfig' },
+  { 'VonHeikemen/lsp-zero.nvim' },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'L3MON4D3/LuaSnip',         -- Snippet engine
+      'saadparwaiz1/cmp_luasnip', -- Snippet engine adapter
+      'hrsh7th/cmp-nvim-lsp',     -- Source for LSP completion
+    }
+  },
+  'mfussenegger/nvim-jdtls',
   'stevearc/conform.nvim',
-  'windwp/nvim-ts-autotag'
-}
+  'windwp/nvim-ts-autotag',
+  'github/copilot.nvim',
 
+  -- Debugging
+  'mfussenegger/nvim-dap',
+  { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
+}
