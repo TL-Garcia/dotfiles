7367aba7809e610111528004166618ce1c38c342
tl-garcia <tomas.bl.garcia@gmail.com>
2025-04-07 13:45:26 +0200

    nvim: Remove java lsp config

--- nvim/.config/nvim/ftplugin/java.lua
+++
@@ -1,7 +0,0 @@
-require 'jdtls'.start_or_attach({
-  cmd = {
-    vim.fn.expand '$HOME/.local/share/nvim/mason/bin/jdtls',
-    ('--jvm-arg=-javaagent:%s'):format(vim.fn.expand '$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar')
-  },
-  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
-})
