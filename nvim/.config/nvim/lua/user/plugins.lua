deeb4d4908c6fdb8328f2410b95b121760daf7c3
tl-garcia <tomas.bl.garcia@gmail.com>
2025-07-04 14:33:32 +0200

    nvim: Remove copilot plugin

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -65,28 +65,6 @@ require('lazy').setup {
       require("lsp-file-operations").setup()
     end,
   },
-  {
-    "github/copilot.nvim",
-    event = "InsertEnter",
-    lazy = false,
-    autoStart = true,
-    build = ":Copilot auth",
-    url = "git@github.com:github/copilot.vim.git",
-    config = function()
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_enabled = false
-      vim.keymap.set("n", "<leader>ai", function()
-        vim.g.copilot_enabled = not vim.g.copilot_enabled
-        if vim.g.copilot_enabled then
-          print("Copilot enabled")
-        else
-          print("Copilot disabled")
-        end
-      end, { desc = "Toggle Copilot" })
-      vim.api.nvim_set_keymap("i", "<C-L>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-      vim.g.copilot_no_tab_map = true
-    end,
-  },
 
   -- Debugging
   'mfussenegger/nvim-dap',
